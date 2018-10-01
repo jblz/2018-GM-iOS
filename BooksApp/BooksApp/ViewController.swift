a616ed390001a73602327a2f219ded17ecb82c24
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-01 10:03:09 -0400

    Add project skeleton

---
+++ BooksApp/BooksApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BooksApp
+//
+//  Created by Cesar Tardaguila on 1/10/2018.
+//  Copyright © 2018 automattic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
