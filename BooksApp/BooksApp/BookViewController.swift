dc2e02b47c3dab47788951948e8ee869448cb3cc
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-02 11:22:05 -0400

    Add BookViewModel

--- BooksApp/BooksApp/BookViewController.swift
+++ BooksApp/BooksApp/BookViewController.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class BookViewController: UIViewController {
+struct BookViewModel: CustomStringConvertible {
+    let title: String
+    let subtitle: String
+    let author: String
+    let extendedDescription: String
+
+    var description: String {
+        return "I am a book, my title is \(title)"
+    }
+}
+
+final class BookViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
@@ -16,22 +27,32 @@ class BookViewController: UIViewController {
     @IBOutlet weak var coverImageView: UIImageView!
     @IBOutlet weak var descriptionTextView: UITextView!
 
+    let book = BookViewModel(title: "Moby Dick", subtitle: "A book about a whale", author: "Herman Melville", extendedDescription: "This is a book about a man tryin gto hunt a whale.")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        assignTitle()
+        assignSubtitle()
+        assignAuthor()
+        assignDescription()
+
+        print(book)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func assignTitle() {
+        titleLabel.text = book.title
     }
-    */
 
+    private func assignSubtitle() {
+        subtitleLabel.text = book.subtitle
+    }
+
+    private func assignAuthor() {
+        authorLabel.text = book.author
+    }
+
+    private func assignDescription() {
+        descriptionTextView.text = book.extendedDescription
+    }
 }
