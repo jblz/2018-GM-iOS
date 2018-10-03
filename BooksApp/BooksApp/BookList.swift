2a8fb8691326421a695c565d883aab59baeca67a
Jeff Bowen <jeff@automattic.com>
2018-10-03 16:23:51 -0400

    add error handling

--- BooksApp/BooksApp/BookList.swift
+++ BooksApp/BooksApp/BookList.swift
@@ -31,14 +31,27 @@ class BookList: UITableViewController, UISearchResultsUpdating {
         
         networkRequest.get(url: url) { (books, error) in
             if let error = error {
-                print(error)
+                DispatchQueue.main.async {
+                    self.showError(error)
+                }
                 return
             }
+
             self.books = books ?? []
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
+    }
+    
+    private func showError(_ error: Error) {
+        let description = error.localizedDescription;
+
+        let alertController = UIAlertController(title: "Error", message: description, preferredStyle: .alert)
+
+        let dismissAction = UIAlertAction(title: "OK", style: .cancel)
+        alertController.addAction(dismissAction)
+        present(alertController, animated: true)
     }
     
     private func insertSearchController() {
